8f85222d60d6995ba32a04d283d2a98caf0ddeb9
Champion31415926 <2470865290@qq.com>
2025-10-31 12:43:39 +0800

    Dashboard statistics interface, related to #13

---
+++ src/main/java/cn/mrblackpineapple/trading_website/vo/ChartVO.java
@@ -0,0 +1,12 @@
+package cn.mrblackpineapple.trading_website.vo;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+public class ChartVO {
+
+    private String name;
+
+    private Integer count;
+}
