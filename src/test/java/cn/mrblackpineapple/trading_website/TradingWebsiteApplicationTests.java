16ca6e0d6ec49453d68f5729a70c973fa1e23e77
Champion31415926 <2470865290@qq.com>
2025-11-14 17:11:55 +0800

    Reload Test file

--- src/test/java/cn/mrblackpineapple/trading_website/TradingWebsiteApplicationTests.java
+++ src/test/java/cn/mrblackpineapple/trading_website/TradingWebsiteApplicationTests.java
@@ -1,12 +1,5 @@
 package cn.mrblackpineapple.trading_website;
 
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-
-@SpringBootTest
-@ActiveProfiles("test")
 public class TradingWebsiteApplicationTests {
-    @org.junit.jupiter.api.Test
-    void contextLoads() {
-    }
+
 }
