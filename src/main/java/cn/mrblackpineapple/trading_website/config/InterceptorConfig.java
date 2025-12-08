5b74b5a452513aab9ffbfbbfaf780908da043b08
Champion31415926 <2470865290@qq.com>
2025-12-08 16:32:08 +0800

    Authentication interceptor, related to #11

---
+++ src/main/java/cn/mrblackpineapple/trading_website/config/InterceptorConfig.java
@@ -0,0 +1,26 @@
+package cn.mrblackpineapple.trading_website.config;
+
+import cn.mrblackpineapple.trading_website.interceptor.JwtInterceptor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+
+    @Value("${my-server.api-context-path}")
+    private String API;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new JwtInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        API + "/user/login",
+                        API + "/user/register",
+                        API + "/file/upload",
+                        API + "/file/getFile"
+                );
+    }
+}
